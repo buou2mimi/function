618bc7c1c0856c2fe7b303101ee9e791a0b7334d
buou2mimi <903016537@qq.com>
2019-04-26 10:33:54 +0800

    函数的调用

---
+++ function13.py
@@ -0,0 +1,8 @@
+# _*_ coding :UTF-8 _*_
+# 开发人员：C
+# 开发时间：2019/4/26 10:04
+# 文件名称：function13.py
+# 开发软件：PyCharm
+from user_profile import build_profile
+my_information=build_profile('cui','yaxie',height='173cm',weight='55kg',field='conterl engineering')
+print(my_information)
--- user_profile.py
+++ user_profile.py
@@ -11,5 +11,5 @@ def build_profile(first,last,**user_info):
     for key,value in user_info.items():
         profile[key]=value
     return profile
-user_profile=build_profile('albert','einstein',location='princeton',field='physics')
-print(user_profile)
+#user_profile=build_profile('albert','einstein',location='princeton',field='physics')
+#print(user_profile)
