59e08ffc209f0b1d5886273162d5c5d02340abd0
buou2mimi <903016537@qq.com>
2019-04-26 15:02:19 +0800

    调用函数printing_functions

---
+++ printing_model3.py
@@ -0,0 +1,10 @@
+# _*_ coding :UTF-8 _*_
+# 开发人员：C
+# 开发时间：2019/4/26 14:48
+# 文件名称：printing_model3.py
+# 开发软件：PyCharm
+import printing_functions as pf
+unprint_designs=['iphone case','robot pendant','dodecahedron']
+completd_models=[]
+pf.print_models(unprint_designs,completd_models)
+pf.show_completed_models(completd_models)
